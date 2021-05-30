32dd31f0519440aa22480556b3d7adbd2fe542d9
Thomas Neff <thomasjneff@gmail.com>
2021-05-30 14:26:50 -0400

    updated config for cognito

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "us-east-1_PH6JH3z49",
+    userPoolClientId: "6d0ccec54no4vvsu07e0t7cqld",
+    region: "us-east-1",
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
